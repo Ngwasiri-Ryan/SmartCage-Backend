44ca118761ffe60d57c0ab64cf338c715b60c7ac
Ngwasiri Ryan <ryanngwasiri@gmail.com>
2026-06-19 17:15:52 +0100

    chore: delete railway.json to use Dockerfile CMD & bind host to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`SmartCage API running on port ${port}`);
 }
 
